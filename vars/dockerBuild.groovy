de279932eb156012e33c1f4a61b2d64fb6e0fa6b
Amir Subhani <amir.subhani@gmail.com>
2023-09-24 12:38:54 +0530

    docker build

---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,8 @@
+def call (String project, String Imagetag, String hubUser){
+    sh """
+        docker image build -t ${hubUser}/${project} .
+        docker image tag ${hubUser}/${project} ${hubUser}/${project}:${Imagetag}
+        docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest 
+
+    """
+}
