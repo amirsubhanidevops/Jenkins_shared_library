61b473986322297ca542616654f42dbdcb7018f8
Amir Subhani <amir.subhani@gmail.com>
2023-09-24 13:51:24 +0530

    dokcer image build : ECR

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,3 +1,4 @@
+/*
 def call (String project, String Imagetag, String hubUser){
     sh """
         docker image build -t ${hubUser}/${project} .
@@ -5,4 +6,16 @@ def call (String project, String Imagetag, String hubUser){
         docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest 
 
     """
+}
+
+*/
+
+def call (String aws_acc_id, String region, String ecr_name_repo){
+
+    sh """
+     docker build -t ${ecr_name_repo} .
+     docker tag ${ecr_name_repo}:latest ${aws_acc_id}.dkr.ecr.${region} .amazonaws.com/${ecr_name_repo}:latest
+
+    """
+
 }
