f4276d3c52cad2c3802714d0f2b3618e41626140
Amir Subhani <amir.subhani@gmail.com>
2023-09-24 14:12:12 +0530

    all stages added : ECR

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,3 +1,4 @@
+/*
 def call (String project, String Imagetag, String hubUser){
     withCredentials([usernamePassword(
         credentialsId: 'docker', 
@@ -10,4 +11,16 @@ def call (String project, String Imagetag, String hubUser){
 
     sh "docker image push ${hubUser}/${project}:${Imagetag}"
     sh "docker image push ${hubUser}/${project}:latest" 
+}
+
+*/
+
+def call (String aws_acc_id, String region, String ecr_name_repo){
+
+    sh """
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_acc_id}.dkr.ecr.${region}.amazonaws.com
+     docker push ${ecr_name_repo}:latest ${aws_acc_id}.dkr.ecr.${region}.amazonaws.com/${ecr_name_repo}:latest
+
+    """
+
 }
--- vars/dockerimageScan.groovy
+++ vars/dockerimageScan.groovy
@@ -12,7 +12,7 @@ def call (String project, String Imagetag, String hubUser){
 def call (String aws_acc_id, String region, String ecr_name_repo){
 
     sh """
-     trivy image ${ecr_name_repo}:latest ${aws_acc_id}.dkr.ecr.${region} .amazonaws.com/${ecr_name_repo}:latest > scan.txt
+     trivy image ${ecr_name_repo}:latest ${aws_acc_id}.dkr.ecr.${region}.amazonaws.com/${ecr_name_repo}:latest > scan.txt
      cat scan.txt
     """
 
