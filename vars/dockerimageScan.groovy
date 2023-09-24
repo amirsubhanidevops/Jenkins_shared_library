d55660c781f78c09e5ab96562a339d4be848026e
Amir Subhani <amir.subhani@gmail.com>
2023-09-24 14:02:40 +0530

    dokcer image build : ECR

--- vars/dockerimageScan.groovy
+++ vars/dockerimageScan.groovy
@@ -1,7 +1,19 @@
+/*
 def call (String project, String Imagetag, String hubUser){
     sh """
         trivy image ${hubUser}/${project}:latest > scan.txt
         cat scan.txt 
 
     """
+}
+
+*/
+
+def call (String aws_acc_id, String region, String ecr_name_repo){
+
+    sh """
+     trivy image ${ecr_name_repo}:latest ${aws_acc_id}.dkr.ecr.${region} .amazonaws.com/${ecr_name_repo}:latest > scan.txt
+     cat scan.txt
+    """
+
 }
